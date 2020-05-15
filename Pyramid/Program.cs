25859d586b98da2818595e187b8a22f4a1024195
clarelimo <clarelimo5@gmail.com>
2020-05-15 03:32:31 +0300

    first

---
+++ Pyramid/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Pyramid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+        }
+    }
+}
+
+//       *
+//      ***
+//     *****
+//    *******
+//   *********
